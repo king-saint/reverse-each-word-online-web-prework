22fbf70cb367fb71590bd28feb2cf13da342cd6b
Wendi Chen <epanistamai.proles@gmail.com>
2018-11-24 21:09:02 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,11 +5,11 @@ def reverse_each_word(string)
     array.each do |word|
       new_array << word.reverse
     end
-  new_array.join('')
+  new_array.join(' ')
    
     array.collect do |word|
       newer_array << word.reverse
     end
-  newer_array.join('')
+  newer_array.join(' ')
 
 end
